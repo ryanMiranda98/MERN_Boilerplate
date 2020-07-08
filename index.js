2507e994e0b56a04636b23902399dceb93902f8f
unknown <ryanjude.miranda@gmail.com>
2020-07-09 04:21:51 +0530

    created config files

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+const User = require("./models/User");
 
 mongoose
-  .connect(
-    "mongodb+srv://ryan:BIAtLlY62EGXIw4b@react-blog.ka5sh.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/users/register", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      success: false,
+      error: err,
+    });
+  }
 });
 
 app.listen(5000, (req, res) => {
