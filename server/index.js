7a71360b88cdc4bf1a48abc42b5e4f39c917411c
unknown <ryanjude.miranda@gmail.com>
2020-07-09 13:04:35 +0530

    Heroku 2

--- server/index.js
+++ server/index.js
@@ -23,6 +23,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.json({"Hello: I am happy to deploy our application!"});
+});
+
 app.get("/api/users/auth", auth, (req, res) => {
   res.status(200).json({
     _id: req._id,
